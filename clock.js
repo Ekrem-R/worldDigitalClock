c9a30f027ac26cc2d5c2d0602d8bed6c57b9a530
Ekrem-R <gurbetsila.5259@gmail.com>
2021-01-29 19:09:40 +0000

    first commit

---
+++ clock.js
@@ -0,0 +1,36 @@
+
+   let clickCityElement1 = document.getElementById("tokyo");
+   clickCityElement1.addEventListener("click", displayLocationTimeTokyo);
+   let clickCityElement2 = document.getElementById("london");
+   clickCityElement2.addEventListener("click", displayLocationTimeLondon);
+   let clickCityElement3 = document.getElementById("istanbul");
+   clickCityElement3.addEventListener("click", displayLocationTimeIstanbul);
+   let zurih1 = setInterval(currentTimeLocation, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ function.js
@@ -0,0 +1,65 @@
+function timeCheck(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function displayLocationTimeTokyo() {
+    let dateTime = new Date().toLocaleString("en-UK", {
+        timeZone: "Asia/Tokyo"
+    });
+    dateTime = new Date(dateTime);
+    let location = "Tokyo";
+    let hours = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+    hours = timeCheck(hours);
+    minutes = timeCheck(minutes);
+    seconds = timeCheck(seconds);
+    document.getElementById("locationTime").innerHTML = hours + ":" + minutes + ":" + seconds + "  " + location;
+    clearInterval(zurih1);
+}
+
+function displayLocationTimeLondon() {
+    let dateTime = new Date().toLocaleString("en-UK", {
+        timeZone: "Europe/London"
+    });
+    dateTime = new Date(dateTime);
+    let location = "London";
+    let hours = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+    hours = timeCheck(hours);
+    minutes = timeCheck(minutes);
+    seconds = timeCheck(seconds);
+    document.getElementById("locationTime").innerHTML = hours + ":" + minutes + ":" + seconds + "  " + location;
+    clearInterval(zurih1);
+}
+
+function displayLocationTimeIstanbul() {
+    let dateTime = new Date().toLocaleString("en-UK", {
+        timeZone: "Europe/Istanbul"
+    });
+    dateTime = new Date(dateTime);
+    let location = "Istanbul";
+    let hours = dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+    hours = timeCheck(hours);
+    minutes = timeCheck(minutes);
+    seconds = timeCheck(seconds);
+    document.getElementById("locationTime").innerHTML = hours + ":" + minutes + ":" + seconds + "  " + location;
+    clearInterval(zurih1);
+}
+
+function currentTimeLocation() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    hours = timeCheck(hours);
+    minutes = timeCheck(minutes);
+    seconds = timeCheck(seconds);
+    document.getElementById("locationTime").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
